edc42a1369834578eaeb1b7b70ef6d972ccb825f
Pavel <example@gmail.com>
2023-02-26 20:02:55 +0700

    adaptationadaptation

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,15 @@ $(document).ready(function(){
 
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icon/chevron_carousel/chevron_left.png" alt="left"/></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron_carousel/chevron_right.png" alt="right"></button>',
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 991,
                 settings: {
                     arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]

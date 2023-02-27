68712fb0aabdef966ae3e6646fd475b25aafedb7
PavelMr42 <Pashagrigorev403@gmail.com>
2023-02-27 21:04:52 +0700

    email server

--- src/js/script.js
+++ src/js/script.js
@@ -84,5 +84,24 @@ $(document).ready(function(){
 
     // Masked input
 
-    $('input[name=phone]').mask("+7(999) 999-99-99");
+        $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+        $('form').submit(function(e) {
+            e.preventDefault();
+            if (!$(this).valid()) {
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #gratitude').fadeIn('slow');
+
+                $('form').trigger('reset');
+            });
+            return false;
+        });
 });
